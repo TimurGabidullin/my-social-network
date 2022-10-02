037ce61a731b67aa9347974608a1c841c0ea60ea
TimurGabidullin <pr.Gabidullin@gmail.com>
2022-10-02 19:16:08 +0400

    refactored css 10/02/22

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -21,32 +21,30 @@ let User: React.FC<UsersPropsType> = (props) => {
         unfollow,
         followingInProgress,
     } = props
-    
+
     return <div className={styles.user}>
-                <div>
-                    <NavLink to={'/profile/' + user.id}><img src={user.photos.small !== null ? user.photos.small : userPhoto}
-                                                          className={styles.userPhoto}/></NavLink>
-                </div>
+        <div>
+            <NavLink to={'/profile/' + user.id}><img src={user.photos.small !== null ? user.photos.small : userPhoto}
+                                                     className={styles.userPhoto}/></NavLink>
+        </div>
 
 
+        <div>
+            {user.followed
+                ? <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => {
+                    unfollow(user.id)
 
-
-
-                <div>
-                    {user.followed
-                        ? <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => {
-                            unfollow(user.id)
-
-                        }}>Unfollow</button>
-                        : <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => {
-                            follow(user.id)
-                        }}>Follow</button>}
-                </div>
-                <div>{user.name}</div>
-                <div>{user.status}</div>
-                <div>{"u.location.country"}</div>
-                <div>{"u.location.city"}</div>
-
-            </div>
+                }}>Unfollow</button>
+                : <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => {
+                    follow(user.id)
+                }}>Follow</button>}
+        </div>
+        <div className={styles.userInfo}>
+            <div>{user.name}</div>
+            <div>{user.status}</div>
+            <div>{"u.location.country"}</div>
+            <div>{"u.location.city"}</div>
+        </div>
+    </div>
 }
 export default User
